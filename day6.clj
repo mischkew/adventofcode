a99ac1edb28b48f9eab20b50a11c7cda1e6f8200
Sven Mkw <sven.mkw@gmail.com>
2015-12-10 01:06:30 +0100

    challenge1/day6

---
+++ day6.clj
@@ -0,0 +1,68 @@
+;; learn something about 2dimensional datastructures in clojure...
+(require '[clojure.string :as str])
+(use 'clojure.java.io)
+(def filename "/Users/sven/Documents/workspace/adventofcode/day6-input.txt")
+
+(defn compute-from-input [filename cb]
+  (with-open [rdr (reader filename)]
+    (cb (line-seq rdr))))
+
+(defn build-grid [sizeX sizeY]
+  (loop [grid (transient []) y 0]
+    (if (= y sizeY)
+      (persistent! grid)
+      (recur (conj! grid (vec (map (fn [_] :off) (range sizeX)))) (inc y)))))
+       
+(defn operation [string]
+  (cond (.contains string "turn on") :on
+        (.contains string "turn off") :off
+        (.contains string "toggle") :toggle))
+
+(defn update-operation [operation-key current]
+  (if (= operation-key :toggle) (if (= current :on) :off :on)
+      operation-key))
+              
+(defn region [string]
+  (let [v (vec (map read-string (re-seq #"\d+" string)))]
+    {:min {:x (get v 0), :y (get v 1)}
+     :max {:x (get v 2), :y (get v 3)}}))
+
+(defn set-region [grid region fnvalue]
+  (defn in-range [value orientation]
+    (and (<= value (-> region :max orientation))
+         (>= value (-> region :min orientation))))
+  (defn set-y-region [grid-y]
+    (vec (for [y (range (count grid-y))
+          :let [op (get grid-y y)]]
+      (if (in-range y :y)
+        (fnvalue op)
+        op))))
+  (defn set-x-region []
+    (vec (for [x (range (count grid))
+          :let [grid-y (get grid x)]]
+      (if (in-range x :x)
+        (set-y-region grid-y)
+        grid-y))))
+  (set-x-region))
+
+(defn apply-instruction [grid instruction-string]
+  (set-region grid
+              (region instruction-string)
+              (partial update-operation (operation instruction-string))))
+
+(defn count-lit-helper [grid]
+  (count (for [x (flatten grid)
+               :when (= x :on)] x)))
+
+(defn count-lit []
+  (compute-from-input
+   filename
+   (fn [lines]
+     (loop [instructions lines
+            grid (build-grid 1000 1000)]
+       (if (empty? instructions)
+         (count-lit-helper grid)
+         (recur (rest instructions) (apply-instruction grid (first instructions))))))))
+
+;; (count-lit)
+;; --> 377891
