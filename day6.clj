137ef16d33a5831a557cae5835967083a22193c7
Sven Mkw <sven.mkw@gmail.com>
2015-12-12 17:36:34 +0100

    challenge2/day6

--- day6.clj
+++ day6.clj
@@ -1,26 +1,31 @@
 ;; learn something about 2dimensional datastructures in clojure...
+
+;; == IMPORTS ==
+
 (require '[clojure.string :as str])
 (use 'clojure.java.io)
+
+
+;; == HELPERS ==
+
 (def filename "/Users/sven/Documents/workspace/adventofcode/day6-input.txt")
 
 (defn compute-from-input [filename cb]
   (with-open [rdr (reader filename)]
     (cb (line-seq rdr))))
 
-(defn build-grid [sizeX sizeY]
+;; == DAY 6 HELPERS ==
+
+(defn build-grid [sizeX sizeY value]
   (loop [grid (transient []) y 0]
     (if (= y sizeY)
       (persistent! grid)
-      (recur (conj! grid (vec (map (fn [_] :off) (range sizeX)))) (inc y)))))
+      (recur (conj! grid (vec (map (fn [_] value) (range sizeX)))) (inc y)))))
        
 (defn operation [string]
   (cond (.contains string "turn on") :on
         (.contains string "turn off") :off
         (.contains string "toggle") :toggle))
-
-(defn update-operation [operation-key current]
-  (if (= operation-key :toggle) (if (= current :on) :off :on)
-      operation-key))
               
 (defn region [string]
   (let [v (vec (map read-string (re-seq #"\d+" string)))]
@@ -45,6 +50,13 @@
         grid-y))))
   (set-x-region))
 
+
+;; == CHALLENGE 1 ==
+
+(defn update-operation [operation-key current]
+  (if (= operation-key :toggle) (if (= current :on) :off :on)
+      operation-key))
+
 (defn apply-instruction [grid instruction-string]
   (set-region grid
               (region instruction-string)
@@ -59,10 +71,42 @@
    filename
    (fn [lines]
      (loop [instructions lines
-            grid (build-grid 1000 1000)]
+            grid (build-grid 1000 1000 :off)]
        (if (empty? instructions)
          (count-lit-helper grid)
          (recur (rest instructions) (apply-instruction grid (first instructions))))))))
 
 ;; (count-lit)
 ;; --> 377891
+
+
+;; == CHALLENGE 2 ==
+
+(defn update-count [operation current]
+  (cond (= :on operation) (inc current)
+        (= :off operation) (max (dec current) 0)
+        (= :toggle operation) (+ current 2)))
+
+(defn sum-brightness [grid]
+  (reduce (fn [brightness value]
+            (+ brightness value))
+          0
+          (flatten grid)))
+
+(defn count-instruction [grid instruction-string]
+  (set-region grid
+              (region instruction-string)
+              (partial update-count (operation instruction-string))))
+
+(defn count-brightness []
+  (compute-from-input
+   filename
+   (fn [lines]
+     (loop [instructions lines
+            grid (build-grid 1000 1000 0)]
+       (if (empty? instructions)
+         (sum-brightness grid)
+         (recur (rest instructions) (count-instruction grid (first instructions))))))))
+
+;; (count-brightness)
+;; --> 14110788
